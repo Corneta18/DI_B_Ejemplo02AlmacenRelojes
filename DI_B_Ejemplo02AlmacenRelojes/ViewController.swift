f276eb87f8701b46b5978dd634b0ad7d83000ba9
AlejandroBen <alejandrojulio.benavent.pallardo@cieep.com>
2022-11-25 13:36:45 +0100

    Initial Commit

---
+++ DI_B_Ejemplo02AlmacenRelojes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI_B_Ejemplo02AlmacenRelojes
+//
+//  Created by Alejandro Benavent Pallardo on 25/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
